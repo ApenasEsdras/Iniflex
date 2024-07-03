c999c1a9247ddbd92825bb336a0ff2ffc4d067fb
esdras.de.oliveira <apenasesdras@gmail.com>
2024-07-03 16:24:29 -0300

    reenviados as modificações

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import modules.funcionario.Funcionario;
 
 import java.math.BigDecimal;
@@ -14,41 +13,6 @@ public class Main {
 
         for (Funcionario funcionario : funcionarios){
             System.out.println(funcionario);
-=======
-import java.time.format.DateTimeFormatter;
-import java.math.BigDecimal;
-import java.time.LocalDate;
-import java.time.Period;
-import java.util.*;
-import java.util.stream.Collectors;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.printf("Nome: ");
-
-    }
-
-
-    class Pessoa {
-        private String nome;
-        private LocalDate dataNascimento;
-
-        public Pessoa(String nome, LocalDate dataNascimento) {
-            this.nome = nome;
-            this.dataNascimento = dataNascimento;
-        }
-
-        public String getNome() {
-            return nome;
-        }
-
-        public LocalDate getDataNascimento() {
-            return dataNascimento;
-        }
-
-        public int getIdade() {
-            return Period.between(dataNascimento, LocalDate.now()).getYears();
->>>>>>> be9f004aed1b702d31a6152a6d6aa2dd2dc0c53a
         }
     }
 }
